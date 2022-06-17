b2a0236f7f73b8ceea377c860789ba9f87074b38
Pranaya Shrestha <55268350+pshrest2@users.noreply.github.com>
2022-06-17 12:24:24 -0500

    updated docker-compose file. updated github action workflow. add producer and consumer config

---
+++ Dtos/KafkaConfigurationDto.cs
@@ -0,0 +1,15 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSMessageProcessor.Dtos
+{
+    public class KafkaConfigurationDto
+    {
+        public ProducerConfig ProducerConfig { get; set; }
+        public ConsumerConfig ConsumerConfig { get; set; }
+    }
+}
--- ServiceCollectionExtensions.cs
+++ ServiceCollectionExtensions.cs
@@ -3,17 +3,21 @@ using RSMessageProcessor.Kafka.Interface;
 using Microsoft.Extensions.DependencyInjection;
 using System.Linq;
 using System.Reflection;
+using RSMessageProcessor.Dtos;
 
 namespace RSMessageProcessor
 {
     public static class ServiceCollectionExtension
     {
-        public static IServiceCollection AddMessageProcessor(this IServiceCollection services)
+        public static IServiceCollection AddMessageProcessor(this IServiceCollection services, KafkaConfigurationDto config)
         {
             if (!services.Any(descriptor => descriptor.ServiceType.Assembly == Assembly.GetExecutingAssembly()))
             {
-                services.AddTransient(typeof(IKafkaConsumer<,>), typeof(KafkaConsumer<,>));
-                services.AddTransient(typeof(IKafkaProducer<,>), typeof(KafkaProducer<,>));
+                services.AddSingleton(config.ProducerConfig);
+                services.AddSingleton(config.ConsumerConfig);
+
+                services.AddSingleton(typeof(IKafkaConsumer<,>), typeof(KafkaConsumer<,>));
+                services.AddSingleton(typeof(IKafkaProducer<,>), typeof(KafkaProducer<,>));
             }
 
             return services;
